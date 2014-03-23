992fe31b53983a95fda079bba7635f0f9a13773d
Qosdil A <qosdil@gmail.com>
2014-03-24 06:06:26 +0700

    Add Setting model.

---
+++ src/models/Setting.php
@@ -0,0 +1,13 @@
+<?php namespace Webarq\Site;
+class Setting extends \Eloquent {
+	
+	public $timestamps = false;
+	
+	public function scopeOfCodeType($query, $code, $type)
+	{
+		return $query->whereCode($code)
+			->whereType($type)
+			->first();
+	}
+
+}
