af73ac5d50bfc4d07b41161f339b72ff07c7e3c5
Qosdil A <qosdil@gmail.com>
2014-04-07 15:54:21 +0700

    Comply a model's change from Admin package.

--- src/models/User.php
+++ src/models/User.php
@@ -5,7 +5,7 @@ class User extends \User {
 	// We shouldn't make this because Admin is a different package
 	public function admin()
 	{
-		return $this->hasOne('\Webarq\Admin\Model\Admin');
+		return $this->hasOne('\Webarq\Admin\User');
 	}
 	
 }
