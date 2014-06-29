5940f22c8d6f3fa3ab12b452529ed8ec09d9e0fa
Qosdil A <qosdil@gmail.com>
2014-06-30 02:52:29 +0700

    BaseController() -> Controller().

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -1,37 +1,14 @@
 <?php namespace Webarq\Site;
 
-class BaseController extends \Controller {
+/**
+* @deprecated
+*
+* Please use Controller instead of this
+*/
+class BaseController extends Controller {
 	
-	protected $ajaxResponse = array();
-	protected $layout = 'layouts.master';
-	
-	public function __construct() {}
-
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = \View::make($this->layout);
-		}
-	}
-	
-	public function __destruct()
-	{
-		if ($this->ajaxResponse)
-		{
-			// Let's prevent requests without 'xmlhttprequest' on production environment
-			if (\App::environment() === 'production' && ! \Request::ajax())
-			{
-				\App::abort(403);
-			}
-			
-			return \Response::json($this->ajaxResponse);
-		}
+	public function __construct() {
+		parent::__construct();
 	}
 
 }
