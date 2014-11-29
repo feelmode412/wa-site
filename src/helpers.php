25893f5ac3c0b692dfd9c2e76b29ec0b18e5086b
Qosdil A <qosdil@gmail.com>
2014-11-30 00:43:59 +0700

    Add 'function_exists' before global function declarations to make it work with PHPUnit.

--- src/helpers.php
+++ src/helpers.php
@@ -9,81 +9,96 @@
 | 
 |
 */
-function __($key, $options = array())
+if ( ! function_exists('__'))
 {
-	$t = (Config::get('i18n.'.App::getLocale().'.'.$key)) ?: $key;
-	foreach ($options as $key => $value)
+	function __($key, $options = array())
 	{
-		$t = str_replace($key, $value, $t);
-	}
-	return $t;
-}
-
-function append_current_url($options = array())
-{
-	parse_str($_SERVER['QUERY_STRING'], $parsedStr);
-	$queryStrings = array_merge($parsedStr, $options);
-
-	return \URL::current().'?'.http_build_query($queryStrings);
-}
-
-function currency_format($number, $currency = 'IDR')
-{
-	return $currency.' '.number_format($number, 2, '.', ',');
-}
-
-function dd2($var)
-{
-	if (in_array(gettype($var), array('boolean', 'NULL')))
-		var_dump($var);
-	else
-	{
-		echo '<pre>';
-		print_r($var);
-	}
-
-	die;
-}
-
-function handle_upload($inputName, $prefix, $row = null, $resizeWidth = null, $resizeHeight = null, $ratio = true)
-{
-	if (Input::hasFile($inputName))
-	{
-		$path = public_path('contents').'/';
-		$file = Input::file($inputName);
-		if (strlen($prefix) > 16)
+		$t = (Config::get('i18n.'.App::getLocale().'.'.$key)) ?: $key;
+		foreach ($options as $key => $value)
 		{
-			throw new Exception('Prefix length may not be more than 16.');
+			$t = str_replace($key, $value, $t);
 		}
+		return $t;
+	}
+}
 
-		$fileName = $prefix.'-'.Str::random().'.'.$file->getClientOriginalExtension();
-		if ($resizeWidth || $resizeHeight)
-		{
-			Image::make($file->getRealPath())
-				->resize($resizeWidth, $resizeHeight, $ratio)->save($path.$fileName);
-		}
+if ( ! function_exists('append_current_url'))
+{
+	function append_current_url($options = array())
+	{
+		parse_str($_SERVER['QUERY_STRING'], $parsedStr);
+		$queryStrings = array_merge($parsedStr, $options);
+
+		return \URL::current().'?'.http_build_query($queryStrings);
+	}
+}
+if ( ! function_exists('currency_format'))
+{
+	function currency_format($number, $currency = 'IDR')
+	{
+		return $currency.' '.number_format($number, 2, '.', ',');
+	}
+}
+
+if ( ! function_exists('dd2'))
+{
+	function dd2($var)
+	{
+		if (in_array(gettype($var), array('boolean', 'NULL')))
+			var_dump($var);
 		else
 		{
-			$file->move($path, $fileName);	
+			echo '<pre>';
+			print_r($var);
 		}
-		
-		if ($row && $row->{$inputName} && substr($row->{$inputName}, 0, 8) !== 'default-')
-		{
-			// Use "@" in case the file is missing
-			@unlink($path.$row->{$inputName});
-		}
-		
-		return $fileName;
-	}
-	elseif ($row) // File not uploaded by user on Edit mode
-	{
-		return $row->{$inputName};
-	}
-	else // File not uploaded by user on Add mode
-	{
-		return null;
+
+		die;
 	}
 }
+
+if ( ! function_exists('handle_upload'))
+{
+	function handle_upload($inputName, $prefix, $row = null, $resizeWidth = null, $resizeHeight = null, $ratio = true)
+	{
+		if (Input::hasFile($inputName))
+		{
+			$path = public_path('contents').'/';
+			$file = Input::file($inputName);
+			if (strlen($prefix) > 16)
+			{
+				throw new Exception('Prefix length may not be more than 16.');
+			}
+
+			$fileName = $prefix.'-'.Str::random().'.'.$file->getClientOriginalExtension();
+			if ($resizeWidth || $resizeHeight)
+			{
+				Image::make($file->getRealPath())
+					->resize($resizeWidth, $resizeHeight, $ratio)->save($path.$fileName);
+			}
+			else
+			{
+				$file->move($path, $fileName);	
+			}
+			
+			if ($row && $row->{$inputName} && substr($row->{$inputName}, 0, 8) !== 'default-')
+			{
+				// Use "@" in case the file is missing
+				@unlink($path.$row->{$inputName});
+			}
+			
+			return $fileName;
+		}
+		elseif ($row) // File not uploaded by user on Edit mode
+		{
+			return $row->{$inputName};
+		}
+		else // File not uploaded by user on Add mode
+		{
+			return null;
+		}
+	}
+}
+
 
 /*
 |--------------------------------------------------------------------------
@@ -94,21 +109,26 @@ function handle_upload($inputName, $prefix, $row = null, $resizeWidth = null, $r
 | is "id"
 |
 */
-
-function lang($row, $fieldName)
+if ( ! function_exists('lang'))
 {
-	$locale = App::getLocale();
+	function lang($row, $fieldName)
+	{
+		$locale = App::getLocale();
 
-	return ($locale === 'en')
-		? $row->{$fieldName}
-		: ($row->{$fieldName.'_locale_'.$locale}) ?: $row->{$fieldName};
+		return ($locale === 'en')
+			? $row->{$fieldName}
+			: ($row->{$fieldName.'_locale_'.$locale}) ?: $row->{$fieldName};
+	}
 }
 
-function list_lang($fieldName)
+if ( ! function_exists('list_lang'))
 {
-	$locale = App::getLocale();
+	function list_lang($fieldName)
+	{
+		$locale = App::getLocale();
 
-	return ($locale === 'en')
-		? $fieldName
-		: $fieldName.'_locale_'.$locale;
+		return ($locale === 'en')
+			? $fieldName
+			: $fieldName.'_locale_'.$locale;
+	}	
 }
