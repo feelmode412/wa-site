34ae9ae8fdd547aa67e17cc1a0a069d42d2ab747
Qosdil A <qosdil@gmail.com>
2015-05-30 13:14:17 +0700

    Create migrations.

---
+++ src/migrations/2015_05_30_113536_create_email_templates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailTemplatesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('email_templates', function($table)
+		{
+			$table->increments('id');
+			$table->string('code', 50)->unique();
+			$table->string('title', 50);
+			$table->string('title_locale_id', 50)->nullable()->default(null);
+			$table->text('content');
+			$table->text('content_locale_id')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('email_templates');
+	}
+
+}
---
+++ src/migrations/2015_05_30_114357_create_settings_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('settings', function($table)
+		{
+			$table->increments('id');
+			$table->string('code', 30);
+			$table->string('type', 30);
+			$table->text('value');
+			$table->unique(['code', 'type']);
+		});
+
+		// Insert system data
+		DB::table('settings')->insert([
+			['code' => 'email', 'type' => 'noreply', 'value' => 'noreply@domain.com'],
+			['code' => 'name', 'type' => 'noreply', 'value' => 'Web App X'],
+			['code' => 'footer', 'type' => 'email', 'value' => '<p>&nbsp;<p>\r\n<p>Best regards,</p>'],
+			['code' => 'header', 'type' => 'email', 'value' => 'Dear {email},<br/><br/><br/>'],
+			['code' => 'facebook', 'type' => 'socmed_url', 'value' => 'https://www.facebook.com/webarq'],
+			['code' => 'twitter', 'type' => 'socmed_url', 'value' => 'https://twitter.com/webarq'],
+			['code' => 'youtube', 'type' => 'socmed_url', 'value' => 'http://www.youtube.com/webarq'],
+			['code' => 'static_js', 'type' => 'system', 'value' => 'yes']
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('settings');
+	}
+
+}
