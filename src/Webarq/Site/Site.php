c15b9062fa87094e9acaf8a44f4b7a4f9cfa287f
Qosdil A <qosdil@gmail.com>
2015-05-30 15:32:55 +0700

    handle_upload() -> Site::handleUpload().

--- src/Webarq/Site/Site.php
+++ src/Webarq/Site/Site.php
@@ -21,6 +21,44 @@ class Site {
 		return $currency.' '.number_format($number, 2, '.', ',');
 	}
 
+	public function handleUpload($inputName, $prefix, $row = null, $resizeWidth = null, $resizeHeight = null, $ratio = true)
+	{
+		if (\Input::hasFile($inputName))
+		{
+			$path = public_path('contents').'/';
+			$file = \Input::file($inputName);
+			if (strlen($prefix) > 16)
+			{
+				throw new \Exception('Prefix length may not be more than 16.');
+			}
+
+			$fileName = $prefix.'-'.\Str::random().'.'.$file->getClientOriginalExtension();
+			if ($resizeWidth || $resizeHeight)
+			{
+				\Image::make($file->getRealPath())->resize($resizeWidth, $resizeHeight, $ratio)->save($path.$fileName);
+			}
+			else
+			{
+				$file->move($path, $fileName);	
+			}
+			
+			if ($row && $row->{$inputName} && substr($row->{$inputName}, 0, 8) !== 'default-')
+			{
+				// Use "@" in case the file is missing
+				@unlink($path.$row->{$inputName});
+			}
+			
+			return $fileName;
+		}
+		elseif ($row) // File not uploaded by user on Edit mode
+		{
+			return $row->{$inputName};
+		}
+		else // File not uploaded by user on Add mode
+		{
+			return null;
+		}
+	}
 
 	/**
 	* lang()
