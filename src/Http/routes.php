31d7267784450bc0a7528582b0c2abb0d6479eb7
Qosdil A <qosdil@gmail.com>
2015-07-09 15:12:13 +0700

    CSRF token.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,4 +1,8 @@
 <?php
 Route::group(['prefix' => 'api'], function() {
+    Route::get('csrf-token', function() {
+        return ['_token' => csrf_token()];
+    });
+
     Route::resource('settings', 'Webarq\Site\Http\Controllers\Api\SettingController');
 });
