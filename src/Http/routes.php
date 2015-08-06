b191a0630d911b568dc2581c35730d5572ac58d9
Qosdil A <qosdil@gmail.com>
2015-08-06 18:56:08 +0700

    Add an API resource for global vars.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,26 @@
 <?php
 Route::group(['prefix' => 'api'], function() {
+
+    $adminConfig = config('webarq_admin');
+    Route::get('vars', function() use ($adminConfig) {
+        $content = [
+            'asset' => asset(null),
+            'name' => config('app.name'),
+            'url' => url(),
+        ];
+
+        if ($adminConfig && isset($adminConfig['uri'])) {
+            $content['admin'] = [
+                'asset' => asset($adminConfig['uri']),
+                'url'   => url($adminConfig['uri']),
+            ];
+        };
+
+        return response($content)
+            ->setLastModified(new DateTime('now'))
+            ->setExpires(new DateTime('tomorrow'));
+    });
+
     Route::get('csrf-token', function() {
         return ['_token' => csrf_token()];
     });
